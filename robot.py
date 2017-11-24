e37cb4419fd8e1d9b821c7ea8d4222de6345e5cf
Kasper Dokter <kasperdokter@gmail.com>
2017-11-24 02:27:32 +0100

    fluent movements

--- robot.py
+++ robot.py
@@ -18,11 +18,13 @@ from __future__ import print_function # use python 3 syntax but make it compatib
 from __future__ import division       #                           ''
 
 import io
+import sys
 import time
 import brickpi3
-#import picamera
-#import cv2
+import picamera
+import cv2
 import numpy as np
+import subprocess
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
 
@@ -33,19 +35,25 @@ H = BP.PORT_D
 
 px = 100   # power limit for columns
 sx = 500   # speed limit for columns (degree/second)
-dx = -733  # degrees to move 1 column right
+dx = 710   # degrees to move 1 column right
 
 py = 100   # power for rows
 sy = 500   # speed for rows (degree/second)
-dy = 733   # degrees to move 1 row up
+dy = -710  # degrees to move 1 row up
 
 pz = 100   # power limit for lifting
 sz = 500   # speed limit for lifting (degree/second)
 dz = -900  # angle to lift the piece (degrees)
-tz = 2     # time to lift and lower the grabber (second)
+tz = np.abs(dz/sz)
 
 pu = 100   # power for closing the grabber
-tu = 0.1   # time to close the grabber
+tu = 0.17  # time to close the grabber
+
+# initial position
+x0, y0 = (4, 7)
+
+# current position
+xc, yc = (x0, y0)
 
 def x(d):
     try:
@@ -89,24 +97,56 @@ def h(t, p):
 
 #########################  Functions for robot movement  #################################
 
+def move(m):
+    if len(m) == 4:
+        x1,y1 = A2C(m[:2])
+        x2,y2 = A2C(m[-2:])
+        moveto(x1,y1)
+        grab()
+        moveto(x2,y2)
+        drop()
+        moveto(x0,y0)
+    return
 
-def initialize():
+def init():
     # resets the encoder of the motors and sets the motor limits.
-    BP.set_motor_limits(Z, pz, sz)
-    BP.set_motor_limits(Y, py, sy)
     BP.set_motor_limits(X, px, sx)
+    BP.set_motor_limits(Y, py, sy)
+    BP.set_motor_limits(Z, pz, sz)
+    global xc,yc
     try:
-        BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A))
-        BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
-        BP.offset_motor_encoder(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C))
+        BP.offset_motor_encoder(X, BP.get_motor_encoder(X)-xc*dx)
+        BP.offset_motor_encoder(Y, BP.get_motor_encoder(Y)-yc*dy)
+        BP.offset_motor_encoder(Z, BP.get_motor_encoder(Z))
     except IOError as error:
         print(error)
     return
 	
-def mv(x,y):
+def moveto(x,y):
     # moves the robot to square (x,y) in {0,...,7}^2 = {a,...,h}x{1,...,8}
-    BP.set_motor_position(X, x * dx)
-    BP.set_motor_position(Y, y * dy)
+    
+    # calculate the duration of the move
+    global xc,yc
+    t = 1.2 * np.max([np.abs(x-xc),np.abs(y-yc)])
+    
+    if t > 0:
+        # calculate the speed of each coordinate
+        vx = np.abs((x-xc)*dx/t)
+        vy = np.abs((y-yc)*dy/t)
+        print([t,vx,vy])
+        # set the speed of each motor
+        BP.set_motor_limits(X, px, vx)
+        BP.set_motor_limits(Y, py, vy)
+
+        # move with correct speed to target position
+        BP.set_motor_position(X, x * dx)
+        BP.set_motor_position(Y, y * dy)
+
+        # wait until the move is completed
+        time.sleep(t)
+
+        # update the current position
+        xc, yc = (x, y)
     return
 
 def up():
@@ -149,15 +189,24 @@ def drop():
     up()
     return
 
+def A2C(s):
+    # algebraic notation to cartesian coordinates
+    x,y = (0,0)
+    if len(s) == 2:
+       x = np.max([np.min([ord(s[0])-97,7]),0])
+       y = np.max([np.min([int(s[1])-1,7]),0])
+    return (x,y)
+
 #########################  Main program loop  #################################
 	
 try:
 
     if BP.get_voltage_battery() < 7:
         print("Battery voltage is too low (" + str(BP.get_voltage_battery())  + "V). Exiting.")
-        SafeExit()
+        BP.reset_all()
+        sys.exit()
 
-    initialize()
+    init()
 		
     while True:
         c = str(raw_input("> "))
